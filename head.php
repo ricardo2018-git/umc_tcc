7cc84bece004c1ec70c9082a63770a8d609175b3
ricardo2018-git <rp_mira@hotmail.com>
2018-08-08 23:08:29 -0300

    menu modal entre

---
+++ head.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">							<!--Compatibilidade com internet explore-->
+		<meta name="viewport" content="width=device-width, initial-scale=1">			<!--Define a largura do dispositivo-->
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>UMC - Anatomia</title>
+
+		  <meta name="viewport" content="width=device-width, initial-scale=1">
+		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	
+	<body>
---
+++ menu_superior.php
@@ -0,0 +1,71 @@
+<!-- Busca outra PG -->
+<?php require_once "head.php" ?>
+
+	<!-- Manu superiro -->
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">UMC - Leopoldina</a>
+				</div>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#" data-toggle="modal" data-target="#myModal" ><span class="glyphicon glyphicon-log-in" ></span> Entre</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-user"></span> Cadastre-se</a></li>
+				</ul>
+			</div>
+		</nav>
+	<!-- FIM Manu superiro -->
+
+  <!-- Modal -->
+	  <div class="modal fade" id="myModal" role="dialog">
+	    <div class="modal-dialog">
+	    
+	      <!-- Modal content-->
+	      <div class="modal-content">
+	        <div class="modal-header">
+	          <button type="button" class="close" data-dismiss="modal">&times;</button>
+	          <h4 class="modal-title">Identifique-se</h4>
+	        </div>
+	        <div class="modal-body">
+	       		<form>
+	       			<!-- Img de login -->
+	       				<center><img src="img/login2.jpg" class="img-circle" alt="Cinque Terre" height="150"></center>
+	       			<!-- FIM Img de login -->	
+
+	       			<!-- RGM e sua img -->
+		       			<div class="input-group">
+		       				<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+	    					<input id="id_rgm" type="text" class="form-control" name="rgm" placeholder="Rgm">
+		       			</div>
+	       			<!-- FIM RGM e sua img -->
+	       				<br/>
+	       			<!-- Senha e img -->
+	       				<div class="input-group">
+						    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+						    <input id="id_senha" type="password" class="form-control" name="senha" placeholder="Senha">
+						</div>
+	       			<!-- FIM Senha e img -->
+
+	       			<!-- Botões -->
+	       				<div class="modal-footer row">
+	       					<div class="col-sm-8">
+	       						<button type="button" class="btn btn-success btn-block">Logar</button>
+	       					</div>
+	       					<div class="col-sm-4">
+	       						<button type="button" class="btn btn-warning btn-block">Voltar</button>
+	       					</div>
+	       				</div>
+	       			<!-- FIM Botões -->
+
+	       		</form>
+	        </div>
+	        
+	        <!-- aqui era onde estava o botão de sair -->
+	        
+	      </div>
+	      
+	    </div>
+	  </div>
+  <!-- FIM Modal -->
+
+<!-- Busca outra PG -->
+<?php require_once "roda_pe.php" ?>
