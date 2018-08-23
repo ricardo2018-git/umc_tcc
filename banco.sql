b56ce3a0638e562c27c0e45b3f3c41d4704eea8d
ricardo2018-git <rp_mira@hotmail.com>
2018-08-23 00:18:04 -0300

    inserindo os dados do cliente do banco

--- banco.sql
+++ banco.sql
@@ -11,21 +11,21 @@ CREATE TABLE usuario(
 	nome_usu 		varchar(50) not null,						/*Não pode ser nulo*/
 	email_usu   	varchar(100) not null,						/*Não pode ser nulo*/
 	senha_usu   	varchar(32) not null,						/*Não pode ser nulo*/
-	foto_usu		/*descobrir*/
+	foto_usu		varchar(255),								/*Pode ser null*/
 	data_cad_usu	datetime default current_timestamp			/*Inseri data e hora automatico*/
 );
 
 /* -- Dados do usuario -- */
 CREATE TABLE endereco(
 	id_end      	int not null PRIMARY KEY AUTO_INCREMENT,	/* Gera automatico, chave PK */
-	id_usu			int not null, /*FK*/
+	id_usu			int not null, FOREIGN KEY(id_usu) REFERENCES usuario(id_usu),/*FK*/
 	endereco		varchar(50),
 	complemento 	varchar(30),
-	numero			int,
-	cep				varchar(),
-	cidade			varchar(),
+	#numero			varchar(10),
+	cep				varchar(50),
+	cidade			varchar(50),
 	estado			char(3),
-	telefone		varchar(),
+	telefone		varchar(25),
 	cpf				varchar(12),
 	sexo			varchar(2),
 	nacionalidade	varchar(15)
--- registra_usuario.php
+++ registra_usuario.php
@@ -1,5 +1,9 @@
 <?php
+	
+	#avisa que vamos usar a class
+	require_once('class/db.class.php');
 
+	#Recebe informações cadastrais do usuario
 	$nome 			= $_POST['nome'];
 	$cpf 			= $_POST["cpf"];
 	$email 			= $_POST["email"];
@@ -14,7 +18,30 @@
 	$telefone 		= $_POST["telefone"];
 	$sexo 			= $_POST["sexo"];
 	$nac 			= $_POST["nac"];
+	#foto 			= $_POST["foto"];
 
+	#instancia um obj
+	$objDb = new db();
+	$objDb2 = new db();
 
+	#acessa o metodo de conexao, e recebe a conexao
+	$link = $objDb->conecta_mysql();
+	$link2 = $objDb2->conecta_mysql();
 
+	#query de insert tabela usuario
+	$sql_usuario = " insert into 
+						usuario(rgm_usu, nome_usu, email_usu, senha_usu) 
+							values('$rgm','$nome','$email', '$senha') ";
+
+	#query de insert tabela enderco
+	$sql_endereco =	"insert into 
+						endereco(endereco, complemento, cep, cidade, estado, telefone, cpf, sexo, nacionalidade) 
+							values('$endereco', '$complemento', '$cep', '$cidade', '$estado', '$telefone', '$cpf', '$sexo', '$nac')";
+
+	#execultar a query
+	if(mysqli_query($link, $sql_usuario) && mysqli_query($link2, $sql_endereco)){
+		echo 'Usuario cadastrado com sucesso !';
+	}else{
+		echo 'Erro ao cadastrar usuario.';
+	}
 ?>
