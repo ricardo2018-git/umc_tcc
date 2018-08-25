e3cd0a9ecc436b2c2454ca2cd270b98e44c10d21
ricardo2018-git <rp_mira@hotmail.com>
2018-08-25 00:24:09 -0300

    ajuste para inserir usuario

--- view/menu_superior.php
+++ view/menu_superior.php
@@ -26,7 +26,7 @@
 	          <h4 class="modal-title">Identifique-se</h4>
 	        </div>
 	        <div class="modal-body">
-	       		<form method="POST" action="model/Usuario.class.php">
+	       		<form method="POST" action="view/validar.php">
 	       			<!-- Img de login -->
 	       				<center><img src="img/login2.jpg" class="img-circle" alt="Cinque Terre" height="150"></center>
 	       			<!-- FIM Img de login -->	
