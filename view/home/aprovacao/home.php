8edea85469006ee4b6719f5af1234342fa32366c
ricardo2018-git <rp_mira@hotmail.com>
2018-09-11 07:12:07 -0300

    layout da tela espera pronto

--- view/home/aprovacao/home.php
+++ view/home/aprovacao/home.php
@@ -1,14 +1,3 @@
-<?php 
-	# Inicia session
-	session_start();
-
-	# Verifica se a session não existe
-	if(!isset($_SESSION['nome'])){
-		#redireciona para index passando get 
-		header('Location: ../../../index.php?cadastro=4');
-	}
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
@@ -24,7 +13,7 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 		<!-- Meu css -->
-		<link rel="stylesheet" href="../conf/css/home.css">
+		<!--  <link rel="stylesheet" href="conf/css/home.css">  -->
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -32,6 +21,7 @@
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+
 
 
 	</head>
@@ -51,7 +41,7 @@
 				</div>
 				<div id="navbar" class="navbar-collapse collapse">
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#" data-toggle="modal" data-target="#ent" ><span class="glyphicon glyphicon-log-in" ></span> Sobre</a></li>
+					<li><a href="#" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in" ></span> Sobre</a></li>
 					<li><a href="../sair.php"><span class="glyphicon glyphicon-user"></span> Sair</a></li>
 				</ul>
 			</div>
@@ -60,65 +50,9 @@
 
 	<!-- Corpo -->  
 		<div class="container">
-			<!-- Imagem do usuario -->
-				<div class="row">
-					<div>
-						<?php
-							if($_SESSION['foto'] == null){
 
-								if($_SESSION['sexo'] == 'm'){ # Foto macoluno
-									echo '<img src="../conf/img/usuario_h.png" class="img-circle" alt="Cinque Terre" width=100px>';
+			<?php require_once "menu_lateral.php" ?>
 
-								}else if($_SESSION['sexo'] == 'f'){ # Foto feminino
-									echo '<img src="../conf/img/usuario_f.png" class="img-circle" alt="Cinque Terre" width=100px>';
-
-								}else if($_SESSION['sexo'] == 'a'){	# Foto Outros
-									echo '<img src="../conf/img/usuario_h2.png" class="img-circle" alt="Cinque Terre" width=100px>';
-								}
-							}else{
-								# Pegar foto do banco
-							}
-						?>
-						
-					</div>
-				</div>
-			<!-- FIM Imagem do usuario -->
-
-			<!-- Propaganda -->
-				<div id="propaganda">
-					<!-- RESERVADO PARA PROPRAGANDA -->
-				</div>
-			<!-- FIM Propaganda -->
-
-				<div id="submenu">
-					<ul class="nav nav-tabs">
-						<li class="active"><a data-toggle="tab" href="#espera">Espera</a></li>
-						<li class=""><a data-toggle="tab" href="#aluno">Aluno</a></li>
-						<li class=""><a data-toggle="tab" href="#professor">Professor</a></li>
-						<li class=""><a data-toggle="tab" href="#administracao">ADM...</a></li>
-					</ul>
-					<div class="tab-content">
-						<div id="espera" class="tab-pane fade">
-							<h3>Sala de Espera</h3>
-							<?php
-								
-							?>
-						</div>
-
-						<div id="aluno" class="tab-pane fade">
-							<h3>ALUNO</h3>
-						</div>
-					
-						<div id="professor" class="tab-pane fade">
-							<h3>PROFESSOR</h3>
-						</div>
-					
-						<div id="administracao" class="tab-pane fade">
-							<h3>ADIMINISTRAÇÃO</h3>
-						</div>
-					</div>
-				</div>
-			
 		</div>
 	<!-- Corpo -->  
 
---
+++ view/home/aprovacao/menu_lateral.php
@@ -0,0 +1,341 @@
+<?php 
+  # Inicia session
+  session_start();
+
+  # Verifica se a session não existe
+  if(!isset($_SESSION['nome'])){
+    #redireciona para index passando get 
+    header('Location: ../../../index.php?cadastro=4');
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<style>
+
+body {
+    font-family: "Lato", sans-serif;
+    transition: background-color .5s;
+}
+
+.sidenav {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #111;
+    overflow-x: hidden;
+    transition: 0.5s;
+    padding-top: 60px;
+}
+
+.sidenav a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #818181;
+    display: block;
+    transition: 0.3s;
+}
+
+.sidenav a:hover {
+    color: #f1f1f1;
+}
+
+.sidenav .closebtn {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+    margin-left: 50px;
+}
+
+#main {
+    transition: margin-left .5s;
+    padding: 16px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+
+/*Img do ususario*/
+#img{
+
+  position: relative;
+  left: 0px;
+  top: -20px;
+  width: 102px;
+}
+.desativa {
+  pointer-events: none;
+  cursor: default;
+  opacity: 0.6;
+  text-decoration: line-through;  /*Risca a palavra no meio*/
+}
+
+/* ------------------------------------------------------- */
+.card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    width: 85%;  /*100%*/
+    /*-----------------*/
+    position:relative;
+    left:25%;
+    top:25%;
+}
+.column {
+    float: left;
+    width: 75.0%; /*33.3%*/
+    margin-bottom: 16px;
+    padding: 0 8px;
+}
+@media screen and (max-width: 85%) {  /*650px*/
+    .column {
+        width: 85%;
+        display: block;
+    }
+}
+.container {
+    padding: 0 16px;
+}
+.title {
+    color: grey;
+}
+.button {
+    border: none;
+    outline: 0;
+    display: inline-block;
+    padding: 8px;
+    color: white;
+    background-color: #000;
+    text-align: center;
+    cursor: pointer;
+    width: 100px;
+}
+.button:hover {
+    background-color: #555;
+}
+
+</style>
+
+</head>
+<body>
+<!-- Opções do menu lateral -->
+  <div id="mySidenav" class="sidenav sidebar">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <a href="#"><i class="fa fa-fw fa-home"></i>Inicio</a><hr> <!-- Linha -->
+    <a href="#" onclick="closeNav()">Perfil</a>
+    <a href="#" onclick="closeNav()">Pesquisar</a>
+    <a href="#" onclick="closeNav()">Enviar E-mail</a>
+    <a href="#" onclick="closeNav()">FeedBack</a>
+    <a href="#" onclick="closeNav()" data-toggle="modal" data-target="#myModal">Sobre</a> <!-- chama modal sobre -->
+    <a href="../sair.php" onclick="closeNav()">Saír</a>
+  </div>
+<!-- FIM Opções do menu lateral -->
+
+<div id="main">
+  <!-- <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Iniciar</span>          "antigo iniciar dos caras"-->
+
+<div class="row">
+        <div class="col-md-4">
+
+          <!-- Imagem do usuario -->
+            <div id="img" class="row">
+              <div>
+                <?php
+                  if($_SESSION['foto'] == null){
+
+                    if($_SESSION['sexo'] == 'm'){ # Foto macoluno
+                      echo '<img src="../conf/img/usuario_h.png" class="img-circle" alt="Cinque Terre" width=100px>';
+
+                    }else if($_SESSION['sexo'] == 'f'){ # Foto feminino
+                      echo '<img src="../conf/img/usuario_f.png" class="img-circle" alt="Cinque Terre" width=100px>';
+
+                    }else if($_SESSION['sexo'] == 'o'){ # Foto Outros
+                      echo '<img src="../conf/img/usuario_h2.png" class="img-circle" alt="Cinque Terre" width=100px>';
+                    }
+                  }else{
+                    # Pegar foto do banco
+                  }
+                ?>
+                
+              </div>
+            </div>
+          <!-- FIM Imagem do usuario -->
+
+        </div>
+
+        <div class="col-md-4">
+          <!-- Propaganda -->
+            <div>
+              <!-- RESERVADO PARA PROPRAGANDA -->
+              
+            </div>
+          <!-- FIM Propaganda -->
+        </div>
+        
+        <div class="col-md-4">
+
+        </div>
+      </div>
+
+        <div id="submenu">
+          <ul class="nav nav-tabs">
+            
+            <li class="desativa"><a data-toggle="tab" href="#" onclick="openNav()" style="color:Tomato;">Inicie aqui</a></li>
+            <li class="active"><a data-toggle="tab" href="#espera">Espera</a></li>
+            <li class="desativa"><a data-toggle="tab" href="#aluno">Aluno</a></li>
+            <li class="desativa"><a data-toggle="tab" href="#professor">Professor</a></li>
+            <li class="desativa"><a data-toggle="tab" href="#administracao">ADM...</a></li>
+          </ul>
+
+          <div>
+              <h3>Sala de Espera</h3>
+              <p>Você foi direcionado para sala de espera, ou seja Você tem que aguardar aprovoção do administrador do sistema, isso pode legar alguns dias !!!</p>
+            </div>
+
+          <div class="tab-content">
+                
+            <div id="espera" class="tab-pane fade">
+              <h3>Sala de Espera</h3>
+              <p>Você foi direcionado para sala de espera, ou seja Você tem que aguardar aprovoção do administrador do sistema, isso pode legar alguns dias !!!</p>
+            </div>
+
+            <div id="aluno" class="tab-pane fade">
+              <h3>ALUNO</h3>
+              home fora
+            </div>
+          
+            <div id="professor" class="tab-pane fade">
+              <h3>PROFESSOR</h3>
+              home fora
+            </div>
+          
+            <div id="administracao" class="tab-pane fade">
+              <h3>ADIMINISTRAÇÃO</h3>
+              home fora
+            </div>
+          </div>
+        </div>
+
+        <!-- modal sobre -->
+          <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
+            
+              <!-- Modal content-->
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title">Nossa Equipe</h4>
+                </div>
+                  <div class="modal-body">
+                    
+                    <!-- integrante -->
+                      <div class="row">
+                        <div class="column">
+                          <div class="card">
+                            <img src="../conf/img/equipe/e1.png" alt="Jane" class="mx-auto d-block" style="width:100%">
+                            <div class="container">
+                              
+                              <h2>Gustavo S.</h2>
+                              <p class="title">Analista &amp; Programador</p>
+                              <p>gustavo@example.com</p>
+                              <p><button class="button">Contato</button></p>
+                              
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="column">
+                          <div class="card">
+                            <img src="../conf/img/equipe/e2.png" alt="Jane" style="width:100%">
+                            <div class="container">
+                              
+                              <h2>Rafael M.</h2>
+                              <p class="title">Analista &amp; Programador</p>
+                              <p>rafael@example.com</p>
+                              <p><button class="button">Contato</button></p>
+
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="column">
+                          <div class="card">
+                            <img src="../conf/img/equipe/e3.png" alt="Jane" style="width:100%">
+                            <div class="container">
+                              
+                              <h2>Ricardo R.</h2>
+                              <p class="title">Analista &amp; Programador</p>
+                              <p>ricardo@example.com</p>
+                              <p><button class="button">Contato</button></p>
+                              
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="column">
+                          <div class="card">
+                            <img src="../conf/img/equipe/e4.png" alt="Jane" style="width:100%">
+                            <div class="container">
+                              
+                              <h2>Victor H.</h2>
+                              <p class="title">Analista &amp; Programador</p>
+                              <p>victorh@example.com</p>
+                              <p><button class="button">Contato</button></p>
+
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="column">
+                          <div class="card">
+                            <img src="../conf/img/equipe/e5.png" alt="Jane" style="width:100%">
+                            <div class="container">
+                              
+                              <h2>Victor S.</h2>
+                              <p class="title">Analista &amp; Programador</p>
+                              <p>victors@example.com</p>
+                              <p><button class="button">Contato</button></p>
+
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    <!-- FIM integrante -->
+                  
+                  </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        <!-- FIM modal sobre -->
+<!-- Conteudo -->
+
+</div>
+
+<script>
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
+</script>
+     
+</body>
+</html> 
