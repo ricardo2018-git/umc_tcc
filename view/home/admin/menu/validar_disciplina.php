f3cd894918642995dc5d70df31e62f87abe306d3
ricardo2018-git <rp_mira@hotmail.com>
2018-09-20 00:33:00 -0300

    formatação da tabela de material pendente de aprovação

--- view/home/admin/menu/validar_disciplina.php
+++ view/home/admin/menu/validar_disciplina.php
@@ -6,22 +6,27 @@
 		require_once("../../../model/getDisciplina.php");
 
 		$buscar = new BuscaDisciplina();
-		
+
+		echo '<center><h2>Materias pendentes de aprovação !!!<br><br></h2></center>';
+		echo '<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>Professor</th>
+						<th>Disciplina</th>
+						<th>Assunto</th>
+						<th>Data</th>
+					</tr>
+				</thead>
+				<tbody>';
+
 		foreach($buscar->listarDisciplina() as $key => $row){
-			echo 'Professor: '.$row->getNome_usu();
-			echo '<br>';
-			echo 'Disciplina: '.$row->getDisciplina();
-			echo '<br>';
-			echo 'Assunto: '.$row->getAssunto();
-			echo '<br>';
-			echo 'Titulo: '.$row->getTitulo();
-			echo '<br>';
-			echo 'Conteudo: '.$row->getConteudo();
-			echo '<br>';
-			echo 'Img: '.$row->getImg();
-			echo '<br>';
-			echo 'staus_dis: '.$row->getStaus_dis();
-			echo '<br><br>';
+			echo '<tr><td>' . $row->getNome_usu() 		. '</td>'
+				. '<td>' 	. $row->getDisciplina() 	. '</td>'
+			 	. '<td>' 	. $row->getAssunto() 		. '</td>'
+			 	. '<td>' 	. $row->getData_dis() 		. '</td></tr>';
 		}
+
+		echo '	</tbody>
+			</table>';
 	?>
 </div>
